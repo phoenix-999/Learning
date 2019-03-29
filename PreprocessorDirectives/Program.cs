70a01fc8ab91239c0233f60e2b51c122a0178c6b
y.kalinichenko <y.kalinichenko@KALINICHEN-PC>
2019-03-29 18:21:08 +0200

    beta

---
+++ PreprocessorDirectives/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PreprocessorDirectives
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region
+            Console.WriteLine("region");
+            #endregion
+
+#if DEBUG
+            Console.WriteLine("DEBUG");
+#endif
+
+#line hidden
+            Console.WriteLine("Скрыто от отладчика");
+#line default
+
+#warning Пользовательские предупреждения
+
+//#error Ошибка опрделенная пользователем, не откомпилируется
+        }
+    }
+}
