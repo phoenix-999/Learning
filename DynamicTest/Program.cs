bc505f0c5f565b8ce4ecfe324285d33498566c9d
y.kalinichenko <y.kalinichenko@KALINICHEN-PC>
2019-03-29 14:31:57 +0200

    beta

---
+++ DynamicTest/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace DynamicTest
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            //foreach(var i in Generate()) //Ошибка! Тип i будет object, так как IEnumerable вернет колекцию object на этапе компиляции
+            foreach(dynamic i in Generate()) //Тип будет опознан средой DLR и произойдет DownCast от object к нужному типу на этапе выполнения
+            {
+                Console.WriteLine("Type: {0}, Name: {1}, Age: {2}", i.GetType(), i.Name, i.Age);
+            }
+        }
+
+        static IEnumerable Generate()
+        {
+            yield return new { Name = "Alex", Age = 18 };
+            yield return new { Name = "Yurii", Age = 20 };
+            yield return new { Name = "Myroslav", Age = 23 };
+        }
+    }
+}
---
+++ NestedClassesTest/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedClassesTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            OuterClass.NestedClass nestedClass = new OuterClass.NestedClass();
+            nestedClass.Method();
+        }
+    }
+
+    class OuterClass
+    {
+        private string privateField = "private field";
+        protected string protectedField = "protected field";
+        public string publicField = "public field";
+
+        
+        public void Method()
+        {
+            Console.WriteLine("Outer class{0}", new string('-', 40));
+            NestedClass nestedClass = new NestedClass();
+            //Console.WriteLine(nestedClass.privateField); // не видит вложенного класса
+        }
+
+        public class NestedClass //по умолчанию - private
+        {
+            private string privateField = "private field Nested Class";
+            public void Method()
+            {
+                Console.WriteLine("Nested class{0}", new string('-', 40));
+                OuterClass outerClass = new OuterClass();
+                Console.WriteLine(outerClass.privateField); // видит все поля внешнего класса независимо от модификатора доступа
+                Console.WriteLine(outerClass.protectedField);
+                Console.WriteLine(outerClass.publicField);
+            }
+        }
+    }
+}
