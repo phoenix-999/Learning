13d552182d45218a218cff65871e850c0a6c594a
y.kalinichenko <y.kalinichenko@KALINICHEN-PC>
2019-04-10 16:16:44 +0300

    beta

---
+++ IFormatableTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace IFormattableTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Temparature t = new Temparature(12m);
+            string s = string.Format("{0}", t);
+            Console.WriteLine(s);
+
+            s = string.Format("{0:Celsius}", t); //Симовлы после {0: передаются в IFormattable.ToString
+            Console.WriteLine(s);
+
+            s = string.Format("{0:Fahrenheyt}", t);
+            Console.WriteLine(s);
+
+            s = string.Format("{0:Kelvin}", t);
+            Console.WriteLine(s);
+
+
+            s = t.ToString("kelvin", CultureInfo.CreateSpecificCulture("en-US"));
+            Console.WriteLine(s);
+        }
+    }
+}
---
+++ IFormatableTest/Temparature.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace IFormattableTest
+{
+    class Temparature : IFormattable
+    {
+        private decimal temparature;
+        public decimal Celsius
+        {
+            get
+            {
+                return temparature;
+            }
+        }
+        public decimal Fahrenheyt
+        {
+            get
+            {
+                return temparature * 9 / 5 + 32;
+            }
+        }
+        public decimal Kelvin
+        {
+            get
+            {
+                return temparature + 273.15m;
+            }
+        }
+            
+
+
+        public Temparature(decimal temparature)
+        {
+            this.temparature = temparature;
+        }
+
+        public override string ToString()
+        {
+            return ToString("Celsius", CultureInfo.CurrentCulture);
+        }
+
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            string result = string.Empty;
+
+            if (string.IsNullOrEmpty(format))
+                format = "Celsius";
+
+            if (formatProvider == null)
+                formatProvider = CultureInfo.CurrentCulture;
+
+            switch (format.ToLower())
+            {
+                case "celsius":
+                    result = string.Format("{0} C", Celsius);
+                    break;
+                case "fahrenheyt":
+                    result = string.Format("{0} F", Fahrenheyt);
+                    break;
+                case "kelvin":
+                    result = string.Format("{0} K", Kelvin);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException("Формат {0} не существует в запрошенном представлении.", format);
+            }
+
+            return result;
+        }
+    }
+}
