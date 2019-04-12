28713de3a1096974ca4144cda39df2d41f4a1f5e
y.kalinichenko <y.kalinichenko@KALINICHEN-PC>
2019-04-12 18:00:04 +0300

    beta

--- RegulareExpressions/Program.cs
+++ RegulareExpressions/Program.cs
@@ -13,6 +13,7 @@ namespace RegulareExpressions
         {
             //SearchDigital();
             ReplaceDateWithVariable(DateTime.Now);
+            SearchSubString();
         }
 
         static void SearchDigital()
@@ -30,8 +31,40 @@ namespace RegulareExpressions
         static void ReplaceDateWithVariable(DateTime date)
         {
             string pattern = @"(?<день>\d{1,2})\.(?<месяц>\d{1,2})\.(?<год>\d{2,4})";//Переменные обозначаются <>,а используются {}
+            //Regex.Replace возвращает измененную строку
             string result = Regex.Replace(date.ToShortDateString(), pattern, @"${год}.${месяц}.${день}");//Если $ используется перед выражением - означает взятие переменной
             Console.WriteLine(result);
+
+            //Замена через лямбда-выражение
+            //Возвращаемое значения поиска по регулярному выражению возвращает экземпляр Match, свойство Value которого соответсвует найденному значению
+            //Экземпляр Match передается в лямбда-выражение
+            result = Regex.Replace(
+                "sdfsdfd123sdfsdfd",
+                @"\d+",
+                (Match m) => { return (decimal.Parse(m.Value)+1m).ToString(); }
+                );
+
+            Console.WriteLine(result);
+        }
+
+        static void SearchSubString()
+        {
+            string pattern = @"\d+";
+
+            Regex regex = new Regex(pattern);
+
+            string str = "sfdskflsdlkf123f;gdf;lbjmk4578gmb";
+
+            //Ищет первое вхождение
+            string result = regex.Match(str).Value;
+
+            Console.WriteLine(result);
+
+            //Поиск всех вхождений
+            for (Match m = regex.Match(str); m.Success; m = m.NextMatch())
+            {
+                Console.WriteLine(m.Value);
+            }
         }
     }
 }
