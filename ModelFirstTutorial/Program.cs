808c1de6962e46c6eef9fdbe7e91d4e134f355ff
y.kalinichenko <y.kalinichenko@KALINICHEN-PC>
2019-04-25 17:54:00 +0300

    beta

---
+++ ModelFirstTutorial/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelFirstTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (EFCodeFirstTutorialEntities context = new EFCodeFirstTutorialEntities())
+            {
+                Console.WriteLine("Users:");
+                foreach (Users user in context.Users)
+                {
+                    Console.WriteLine("\t Name = {0}, Age = {1}", user.Name, user.Age);
+                }
+
+                Console.WriteLine("Sareas:");
+                foreach (Sareas sarea in context.Sareas)
+                {
+                    Console.WriteLine("\t SareaName = {0}, UserId = {1}", sarea.SareaName, sarea.User_UserId);
+                }
+            }
+        }
+    }
+}
