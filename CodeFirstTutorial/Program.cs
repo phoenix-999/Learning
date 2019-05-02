00ddd648229564a0c7976a5e569420de3b24477f
y.kalinichenko <y.kalinichenko@KALINICHEN-PC>
2019-05-02 11:46:53 +0300

    beta

--- CodeFirstTutorial/Program.cs
+++ CodeFirstTutorial/Program.cs
@@ -45,6 +45,10 @@ namespace CodeFirstTutorial
                 }
             }
 
+            #region LINQ
+
+            #endregion
+
         }
     }
 }
--- DataBaseFirstTutorial/Program.cs
+++ DataBaseFirstTutorial/Program.cs
@@ -26,13 +26,33 @@ namespace ModelFirstTutorial
             //LINQ
                 var query = from user in context.Users
                             join sarea in context.Sareas on user equals sarea.Users
-                            select new { Name = user.Name, SareaName = sarea.SareaName };
-                foreach (var item in query)
+                            select new Model{ Name = user.Name, SareaName = sarea.SareaName };
+                foreach (Model item in query)
                 {
+                    Console.WriteLine("Name = {0}, SareaName = {1}", item.Name, item.SareaName);
+                    //Попытка изменить значение в БД посредством проекции LINQ
+                    item.Name = "SecondName";
                     Console.WriteLine("Name = {0}, SareaName = {1}", item.Name, item.SareaName);
                 }
 
-                
+                context.SaveChanges();
+
+                //Повторная выборка и проверка изменилось ли значение
+                Console.WriteLine("Users:");
+                foreach (Users user in context.Users)
+                {
+                    Console.WriteLine("\t Name = {0}, Age = {1}", user.Name, user.Age);
+                }
+                //Как и ожидалось - изменение не каснулись БД
+
+            context.Dispose();
+
         }
+    }
+
+    class Model
+    {
+        public string Name { get; set; }
+        public string SareaName { get; set; }
     }
 }
