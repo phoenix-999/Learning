7772cb7b18e79acdfb587c82e5ee113034eb2314
y.kalinichenko <y.kalinichenko@KALINICHEN-PC>
2019-08-23 16:31:35 +0300

    beta

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using LanguageFeatures.Models;
+using LanguageFeatures.Models.Extensions;
 
 namespace LanguageFeatures.Controllers
 {
@@ -18,8 +19,12 @@ namespace LanguageFeatures.Controllers
                 string name = p?.Name;
                 decimal? price = p?.Price;
                 string relatedName = p?.Related?.Name ?? "<None>";
-                results.Add(string.Format("Name: {0}, Price: {1}, Related: {2}", name, price, relatedName));
+                results.Add($"Name: {name}, Price: {price:C2}, Related: {relatedName}");
             }
+
+            ShoppingCart shoppingCart = new ShoppingCart();
+            shoppingCart.Products = Product.GetProducts();
+            results.Add($"Total: {shoppingCart.Products.TotalPrices()}");
 
             return View(results);
         }
---
+++ LanguageFeatures/Models/MyExtensionMethods.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanguageFeatures.Models.Extensions
+{
+    public static class MyExtensionMethods
+    {
+        public static decimal TotalPrices(this IEnumerable<Product> products)
+        {
+            decimal total = 0;
+
+            foreach (var prod in products)
+            {
+                total += prod?.Price ?? 0;
+            }
+
+            return total;
+        }
+    }
+}
