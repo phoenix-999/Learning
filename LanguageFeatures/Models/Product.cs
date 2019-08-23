5bc0064f74075099426a64d014e69e537c6f6688
y.kalinichenko <y.kalinichenko@KALINICHEN-PC>
2019-08-23 13:28:20 +0300

    beta

---
+++ LanguageFeatures/Models/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanguageFeatures.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal? Price { get; set; }
+        public string Category { get; set; } = "Watersports";
+
+        public bool InStock { get; } = true;
+
+        public Product Related { get; set; }
+
+        public Product(bool stock = true)
+        {
+            InStock = stock;
+        }
+
+        public static Product[] GetProducts()
+        {
+            Product kayak = new Product() { Name = "Kayak", Price = 275M, Category = "Water Craft"};
+            Product lifejacket = new Product(false) { Name = "Lifejacket", Price = 48.95M};
+
+            kayak.Related = lifejacket;
+
+
+            return new Product[] { kayak, lifejacket, null };
+        }
+    }
+}
