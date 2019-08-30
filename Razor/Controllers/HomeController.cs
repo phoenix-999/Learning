7f4bef9d7242b6b1566b78c0f17959fe48bd1cb8
y.kalinichenko <y.kalinichenko@KALINICHEN-PC>
2019-08-30 15:33:44 +0300

    beta

---
+++ Razor/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Razor.Models;
+
+namespace Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            Product myProduct = new Product()
+            {
+                ProductID = 1,
+                Name = "Kayak",
+                Description = "A boat for one person",
+                Category = "Watersports",
+                Price = 275m
+            };
+
+            return View(myProduct);
+        }
+    }
+}
