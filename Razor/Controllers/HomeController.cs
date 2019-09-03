aea1ed16dba0ea569935968432029be01c472b84
y.kalinichenko <y.kalinichenko@KALINICHEN-PC>
2019-09-03 12:13:53 +0300

    beta

--- Razor/Controllers/HomeController.cs
+++ Razor/Controllers/HomeController.cs
@@ -11,16 +11,15 @@ namespace Razor.Controllers
     {
         public ViewResult Index()
         {
-            Product myProduct = new Product()
+            Product[] array = new[]
             {
-                ProductID = 1,
-                Name = "Kayak",
-                Description = "A boat for one person",
-                Category = "Watersports",
-                Price = 275m
+                new Product(){Name = "Kayak", Price = 275M }
+                ,new Product(){ Name = "Lifejacket", Price = 48.95M}
+                ,new Product(){ Name = "Soccer ball", Price = 19.50M}
+                ,new Product(){ Name = "Corner flag", Price = 34.95M}
             };
 
-            return View(myProduct);
+            return View(array);
         }
     }
 }
