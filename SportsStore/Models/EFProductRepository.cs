420c5089fdd19e6af8567e009d14c38930268c5e
y.kalinichenko <y.kalinichenko@KALINICHEN-PC>
2019-09-30 16:29:16 +0300

    beta

---
+++ SportsStore/Models/EFProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    public class EFProductRepository : IProductRepository
+    {
+        private ApplicationDbContext ctx;
+
+        public EFProductRepository(ApplicationDbContext context)
+        {
+            this.ctx = context;
+        }
+        public IQueryable<Product> Products => ctx.Products;
+    }
+}
