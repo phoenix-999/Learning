43dfd14da5b69a9e07f64776ed3f46928dfa2e4d
y.kalinichenko <y.kalinichenko@KALINICHEN-PC>
2019-10-03 18:34:37 +0300

    beta

---
+++ UrlsAndRoutes/Controllers/CustomerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UrlsAndRoutes.Models;
+
+namespace UrlsAndRoutes.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Result", new Result { Controller = nameof(CustomerController), Action = nameof(Index)});
+        }
+
+        public IActionResult List()
+        {
+            return View("Result", new Result { Controller = nameof(CustomerController), Action = nameof(List) });
+        }
+    }
+}
---
+++ UrlsAndRoutes/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UrlsAndRoutes.Models;
+
+namespace UrlsAndRoutes.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Result", new Result { Controller = nameof(HomeController), Action = nameof(Index)});
+        }
+    }
+}
