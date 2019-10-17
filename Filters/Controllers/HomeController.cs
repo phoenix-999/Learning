4dee225372309204b575594892d19b89ceb0ac95
yurii <yurii@DESKTOP-Q995I5E>
2019-10-17 15:50:04 +0300

    beta

---
+++ Filters/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Filters.Controllers
+{
+    [RequireHttps]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Message", "This is Index action from Home controller");
+        }
+    }
+}
